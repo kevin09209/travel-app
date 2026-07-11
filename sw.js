dd667e5843557190a919795d8c3db643ca384b73
kevin09209 <105584166+kevin09209@users.noreply.github.com>
2026-07-11 22:32:25 +0800

    更新漢堡選單與介面風格的快取版本

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker：離線快取 app shell；動態資料（Supabase/匯率/地理服務）永遠走網路。
 // 發新版時把 CACHE_VERSION +1，舊快取會在 activate 時清掉。
-const CACHE_VERSION = "v23";
+const CACHE_VERSION = "v24";
 const CACHE_NAME = "travel-app-" + CACHE_VERSION;
 
 const CORE_ASSETS = [
@@ -9,6 +9,7 @@ const CORE_ASSETS = [
   "css/style.css",
   "css/style-core.css",
   "css/travel-mode.css",
+  "css/app-menu-themes.css",
   "js/app.js",
   "js/app-core.js",
   "js/stop-backups.js",
@@ -16,6 +17,7 @@ const CORE_ASSETS = [
   "js/group-backups.js",
   "js/group-ui-tweaks.js",
   "js/travel-mode.js",
+  "js/app-menu-themes.js",
   "js/store.js",
   "js/store-core.js",
   "js/sync.js",
