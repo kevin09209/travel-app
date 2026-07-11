9752e60d192068e2361d63ec4fb97d46989908f8
kevin09209 <105584166+kevin09209@users.noreply.github.com>
2026-07-11 16:32:11 +0800

    載入精簡版備案按鈕版位

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 // 進入點：先載入既有 App，再掛上景點備案 UI。
 export * from "./app-core.js";
 import { initStopBackups } from "./stop-backups.js";
+import { initCompactStopBackupLayout } from "./stop-backups-compact.js";
 
 initStopBackups();
+initCompactStopBackupLayout();
