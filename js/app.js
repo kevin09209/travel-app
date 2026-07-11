685a169797853824218ad30d413afa9448d1ca16
kevin09209 <105584166+kevin09209@users.noreply.github.com>
2026-07-11 17:47:19 +0800

    載入分組備案介面

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-// 進入點：先載入既有 App，再掛上景點備案 UI。
+// 進入點：先載入既有 App，再掛上景點與分組備案 UI。
 export * from "./app-core.js";
 import { initStopBackups } from "./stop-backups.js";
 import { initCompactStopBackupLayout } from "./stop-backups-compact.js";
+import { initGroupBackups } from "./group-backups.js";
 
 initStopBackups();
 initCompactStopBackupLayout();
+initGroupBackups();
